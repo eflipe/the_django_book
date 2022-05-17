5afee7a37dae41bb678049540df395adb3efca9f
eflipe <felipecabaleiro@gmail.com>
2022-05-17 09:05:30 -0300

    [ADD] intro a templates

--- myclub_root/events_app/views.py
+++ myclub_root/events_app/views.py
@@ -13,5 +13,7 @@ def index(request, year=date.today().year, month=date.today().month):
     month_name = calendar.month_name[month]
     title = "MyClub Event Calendar - %s %s" % (month_name, year)
     cal = HTMLCalendar().formatmonth(year, month)
-    mensaje = "<h1>%s</h1><p>%s</p>" % (title, cal)
-    return HttpResponse(mensaje)
+    mensaje = title
+    context_dict = {'title': mensaje, 'cal': cal}
+    template = 'base.html'
+    return render(request, template, context_dict)
