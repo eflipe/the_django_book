90f8a347d79a40ec6773681a37eef6496422315a
eflipe <felipecabaleiro@gmail.com>
2022-05-17 13:51:40 -0300

    [ADD] templates tags, filters. static

--- myclub_root/events_app/views.py
+++ myclub_root/events_app/views.py
@@ -1,3 +1,4 @@
+from .models import Event
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import date
@@ -15,5 +16,12 @@ def index(request, year=date.today().year, month=date.today().month):
     cal = HTMLCalendar().formatmonth(year, month)
     mensaje = title
     context_dict = {'title': mensaje, 'cal': cal}
-    template = 'base.html'
+    template = 'events_app/calendar_base.html'
+    return render(request, template, context_dict)
+
+
+def all_events(request):
+    event_list = Event.objects.all()
+    template = 'events_app/event_list.html'
+    context_dict = {'event_list': event_list}
     return render(request, template, context_dict)
