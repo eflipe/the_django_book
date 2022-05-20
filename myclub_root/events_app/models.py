170028e3d3b1e0c55224955585a8fe7318b1b0f1
eflipe <felipecabaleiro@gmail.com>
2022-05-20 14:05:02 -0300

    [NEW] more about models and queries, chapter 9

--- myclub_root/events_app/models.py
+++ myclub_root/events_app/models.py
@@ -31,10 +31,24 @@ class MyClubUser(models.Model):
 class Event(models.Model):
     name = models.CharField('Event Name', max_length=120)
     event_date = models.DateTimeField('Event Date')
-    venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
-    manager = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+    venue = models.ForeignKey(
+        Venue, blank=True, null=True, on_delete=models.CASCADE)
+    manager = models.ForeignKey(
+        User, blank=True, null=True, on_delete=models.SET_NULL)
     attendees = models.ManyToManyField(MyClubUser, blank=True)
     description = models.TextField(blank=True)
+
+    def event_timing(self, date):
+        if self.event_date > date:
+            return "Event is after this date"
+        elif self.event_date == date:
+            return "Event is on the same day"
+        else:
+            return "Event is before this date"
+
+    @property
+    def name_slug(self):
+        return self.name.lower().replace(' ','-')
 
     def __str__(self):
         name = self.name
